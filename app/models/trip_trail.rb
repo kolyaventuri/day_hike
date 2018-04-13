05541f43422f5eb4de4c919ea8ae628b79b73afc
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-13 10:13:00 -0600

    Specs and required gems

---
+++ app/models/trip_trail.rb
@@ -0,0 +1,3 @@
+class TripTrail < ApplicationRecord
+  
+end
---
+++ spec/models/trip_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe Trip, type: :model do
+  it { is_expected.to have_many(:trip_trails) }
+  it { is_expected.to have_many(:trails).through(:trip_trails) }
+end
