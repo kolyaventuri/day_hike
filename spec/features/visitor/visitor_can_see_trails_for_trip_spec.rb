773dccfd71a81a0ff31ba623f1eea4f412481b16
Kolya Venturi <kolya.venturi@enx3s.com>
2018-04-13 10:21:00 -0600

    Trip index spec

---
+++ spec/features/visitor/visitor_can_see_trails_for_trip_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  context 'when I visit the trips index' do
+    before(:each) do
+      DatabaseCleaner.clean
+    end
+
+    after(:each) do
+      DatabaseCleaner.clean
+    end
+    scenario 'I should see a list of trips' do
+      num_gen_trips = 3
+
+      create_list(:trip, num_gen_trips)
+
+      visit trip_index
+
+      within('.trips') do
+        trips = find('li')
+        expect(trips.length).to be(num_gen_trips)
+      end
+    end
+  end
+end
